65b400fa7f95cc143488a793ec3718457b7eb24f
Dimos Tsouros <dimosts988@gmail.com>
2024-03-15 14:15:52 +0100

    generic feature representation

--- src/classical_nn.py
+++ src/classical_nn.py
@@ -12,6 +12,9 @@ from src.memoization import p_load
 from sklearn.neural_network import MLPClassifier
 
 
+max_dimensions = 4
+max_blocks = 4
+
 class ProblemInstance:
 
     def __init__(self, labels=None, B=list(), X=set()):
@@ -62,65 +65,70 @@ class ProblemInstance:
         # Returns the features associated with constraint c
         features = []
         scope = get_scope(c)
+        arity = len(scope)
 
-        var_name = get_var_name(scope[0])
-        var_name_same = all([(get_var_name(var) == var_name) for var in scope])
+        var_names = [self.var_names.index(get_var_name(scope[i])) for i in range(arity)]
+        var_name_same = all([var_name == var_names[0] for var_name in var_names])
         features.append(var_name_same)
 
         # Global var dimension properties
-        vars_ndims = [get_var_ndims(var) for var in scope]
-        ndims_max = max(vars_ndims)
-
         vars_dims = [get_var_dims(var) for var in scope]
         dim = []
 
         # TODO add the right amount of Trues in features, this needs to be fixed
-        for j in range(ndims_max):
+        for j in range(max_dimensions):
             dim.append([vars_dims[i][j] for i in range(len(vars_dims)) if len(vars_dims[i]) > j])
 
             dimj_has = len(dim[j]) > 0
-            # features.append(dimj_has)
+            features.append(dimj_has)
+            if not dimj_has:
 
-            if dimj_has:
-                dimj_same = all([dim_temp == dim[j][0] for dim_temp in dim[j]])
-                features.append(dimj_same)
-                dimj_max = max(dim[j])
-                features.append(dimj_max)
-                dimj_min = min(dim[j])
-                features.append(dimj_min)
-                dimj_avg = mean(dim[j])
-                features.append(dimj_avg)
-#                dimj_dev = stdev(dim[j])
-                dimj_diff = average_difference(dim[j])
-                features.append(dimj_diff)
-
-            else:
+                # dummy dimension features
                 features.append(True)
                 for i in range(3): features.append(0)
                 features.append(0.0)
 
+                continue
+
+            dimj_same = all([dim_temp == dim[j][0] for dim_temp in dim[j]])
+            features.append(dimj_same)
+            dimj_max = max(dim[j])
+            features.append(dimj_max)
+            dimj_min = min(dim[j])
+            features.append(dimj_min)
+            dimj_avg = mean(dim[j])
+            features.append(dimj_avg)
+            dimj_diff = average_difference(dim[j])
+            features.append(dimj_diff)
+
+            vars_block = [[dim[j][var] // self.dim_divisors[var_names[var]][j][divisor]
+                           if len(self.dim_divisors[var_names[var]][j]) > divisor else -1 for var in range(len(scope))]
+                          for divisor in range(max_blocks)]
+
             # divisors features --- per var_name, per dimension (up to max dimensions), per divisor (up to max divisors)
             # block same, max, min, average, spread
-            # for each variable name (type of variable
-            for var_name in range(len(self.var_names)):
+            # for each variable name (type of variable)
+            #for var_name in range(len(self.var_names)):
                 # for each dimension of that type of variable
-                vars_block = [[dim[j][var] // divisor for var in range(len(scope))
-                               if self.var_names[var_name] == get_var_name(scope[var])]
-                              for divisor in self.dim_divisors[var_name][j]]
+#            vars_block = [[dim[j][var] // divisor for var in range(len(scope))
+#                           if self.var_names[var_name] == get_var_name(scope[var])]
+#                          for divisor in self.dim_divisors[var_name][j]]
 
-                for l in range(len(self.dim_divisors[var_name][j])):
-                    if self.debug_mode:
-                        print(vars_block[l])
-                    block_same = all([vars_block[l][var] == vars_block[l][0] for var in range(len(vars_block[l]))])
-                    features.append(block_same)
-                    block_max = max(vars_block[l])
-                    features.append(block_max)
-                    block_min = min(vars_block[l])
-                    features.append(block_min)
-                    block_avg = mean(vars_block[l])
-                    features.append(block_avg)
-                    block_dev = stdev(vars_block[l])
-                    features.append(block_dev)
+            for l in range(max_blocks):
+                if self.debug_mode:
+                    print(vars_block[l])
+
+
+                block_same = all([vars_block[l][var] == vars_block[l][0] for var in range(len(vars_block[l]))])
+                features.append(block_same)
+                block_max = max(vars_block[l])
+                features.append(block_max)
+                block_min = min(vars_block[l])
+                features.append(block_min)
+                block_avg = mean(vars_block[l])
+                features.append(block_avg)
+                block_diff = average_difference(vars_block[l])
+                features.append(block_diff)
 
         con_in_gamma = -1
         con_relation = c.get_relation()
