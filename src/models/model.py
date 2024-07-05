157422ff6fce5f1cdea6b3e0695acc2197f5aeec
hverhaeghe <helene.verhaeghe27@gmail.com>
2024-07-05 11:20:57 +0200

    variables on the side

--- src/models/model.py
+++ src/models/model.py
@@ -5,49 +5,14 @@ from src.utils.memoization.memoization import p_load
 from src.utils.utils import get_variables_from_constraints, get_var_name, get_var_dims, get_divisors
 
 
-class Model:
-    def __init__(self, name: str, family: str, data_dir: str):
-        self.name = name
-        self.family = family
-        self.data_dir = data_dir
-        self.data_path = os.path.join(self.data_dir, self.family, self.name)
-        self._gamma = None
-        self._bias = None
-        self._ground_truth = None
-        self._variables = None
+class Variables:
+    def __init__(self, variables):
+        self.variables = variables
         self._var_names = None
         self._var_names_set = None
         self._var_dims = None
         self._var_dims_length = None
         self._var_dims_divisors = None
-
-    @property
-    def gamma(self):
-        if self._gamma is None:
-            # TODO retrieve it from gamma at creation
-            # self._gamma = p_load(os.path.join(self.data_path, "gamma.pickle"))
-            bias = self.bias
-            self._gamma = list(set([c.get_relation() for c in bias]))
-        return self._gamma
-
-    @property
-    def bias(self):
-        if self._bias is None:
-            self._bias = p_load(os.path.join(self.data_path, "bias.pickle"))
-        return self._bias
-
-    @property
-    def ground_truth(self):
-        if self._ground_truth is None:
-            self._ground_truth = p_load(os.path.join(self.data_path, "ground_truth.pickle"))
-        return self._ground_truth
-
-    @property
-    def variables(self):
-        if self._variables is None:
-            bias = self.bias
-            self._variables = get_variables_from_constraints(bias)
-        return self._variables
 
     @property
     def var_names(self):
@@ -88,6 +53,56 @@ class Model:
                                        for i in range(len(dims_length))}
         return self._var_dims_divisors
 
+class Model:
+    def __init__(self, name: str, family: str, data_dir: str):
+        self.name = name
+        self.family = family
+        self.data_dir = data_dir
+        self.data_path = os.path.join(self.data_dir, self.family, self.name)
+        self._gamma = None
+        self._bias = None
+        self._bias_map = None
+        self._ground_truth = None
+        self._variables = None
+
+    @property
+    def gamma(self):
+        if self._gamma is None:
+            # TODO retrieve it from gamma at creation
+            # self._gamma = p_load(os.path.join(self.data_path, "gamma.pickle"))
+            bias = self.bias
+            self._gamma = list(set([c.get_relation() for c in bias]))
+        return self._gamma
+
+    @property
+    def bias(self):
+        if self._bias is None:
+            self._bias = p_load(os.path.join(self.data_path, "bias.pickle"))
+        return self._bias
+
+    @property
+    def bias_map(self):
+        if self._bias_map is None:
+            self._bias_map = {}
+            b = self.bias
+            for i in range(len(b)):
+                self._bias_map[str(b[i])] = i
+        return self._bias_map
+
+    @property
+    def ground_truth(self):
+        if self._ground_truth is None:
+            self._ground_truth = p_load(os.path.join(self.data_path, "ground_truth.pickle"))
+        return self._ground_truth
+
+    @property
+    def variables(self):
+        if self._variables is None:
+            bias = self.bias
+            self._variables = Variables(get_variables_from_constraints(bias))
+        return self._variables
+
+
     def gamma_size(self):
         return len(self.gamma)
 
@@ -112,14 +127,15 @@ class Model:
 
 
 if __name__ == "__main__":
-    m = Model("sudoku9", "sudoku", "../../target/data")
+    m = Model("sudoku4", "sudoku", "../../target/data")
     print(m.gamma)
     print(m.bias)
+    print(m.bias_map)
     print(m.ground_truth)
     print(m.variables)
-    print(m.var_names)
-    print(m.var_names_set)
-    print(m.var_dims)
-    print(m.var_dims_length)
-    print(m.var_dims_divisors)
+    print(m.variables.var_names)
+    print(m.variables.var_names_set)
+    print(m.variables.var_dims)
+    print(m.variables.var_dims_length)
+    print(m.variables.var_dims_divisors)
     print(m.get_stats())
