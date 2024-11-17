8a2e930b527c43062c68a6f7749be5afd7a460e5
Verhaeghe Hélène <helene.verhaeghe27@gmail.com>
2024-11-17 22:02:44 +0100

    prior as feature

--- scriptHPC/step4-3-prior-as-multiplier.sh
+++ scriptHPC/step4-3-prior-as-multiplier.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Submission script for Lemaitre4
-#SBATCH --job-name=step4-2-prior-as-feature
-#SBATCH --output=log/log_step4-2-prior-as-feature_%A_%a.txt
+#SBATCH --job-name=step4-3-prior-as-multiplier
+#SBATCH --output=log/log_step4-2-prior-as-multiplier_%A_%a.txt
 #SBATCH --array=1-2400
 #SBATCH --time=02:00:00 # hh:mm:ss
 #
@@ -26,14 +26,14 @@ else # run with job array
 fi
 
 # Step 4:
-# Step 4-2:
+# Step 4-3:
 
 workingdir="../target"
 scriptdir="../src"
 datadir="${workingdir}/data"
 ldir="${workingdir}/logs"
 outputdir="${ldir}/resultsCA"
-configfile="withpriorpafconfig.txt"
+configfile="withpriorpamconfig.txt"
 
 caalgoname=$(awk -F "\t" -v JOBID="$a" '$1==JOBID {print $2}' "${outputdir}/${configfile}")
 id=$(awk -F "\t" -v JOBID="$a" '$1==JOBID {print $8}' "${outputdir}/${configfile}")
@@ -42,19 +42,20 @@ arch=$(awk -F "\t" -v JOBID="$a" '$1==JOBID {print $4}' "${outputdir}/${configfi
 priorname="$(awk -F "\t" -v JOBID="$a" '$1==JOBID {print $5}' "${outputdir}/${configfile}")[${id}]"
 instancenameCA=$(awk -F "\t" -v JOBID="$a" '$1==JOBID {print $6}' "${outputdir}/${configfile}")
 instancenamePRIOR=$(awk -F "\t" -v JOBID="$a" '$1==JOBID {print $7}' "${outputdir}/${configfile}")
+lambda=$(awk -F "\t" -v JOBID="$a" '$1==JOBID {print $9}' "${outputdir}/${configfile}")
 
-resultdir="${outputdir}/${caalgoname}/paf"
+resultdir="${outputdir}/${caalgoname}/pam"
 mkdir -p ${resultdir}
 
-logfile="${resultdir}/log_withprior_paf_p[${priorname}]_b[${instancenamePRIOR}].txt"
+logfile="${resultdir}/log_withprior_pam_${lambda}_p[${priorname}]_b[${instancenamePRIOR}].txt"
 
 priorfile="${workingdir}/models/${arch}/${priorname}.pickle"
 
-# Feature
+# Multiplier
 
-printf "+++ Prior as a feature for CA model ${caalgoname} [complete parameters = ${caalgo}, instance = ${instancenamePRIOR}, prior model = ${priorname}] +++\n"
+printf "+++ Prior as a multiplier for CA model ${caalgoname} [complete parameters = ${caalgo}, instance = ${instancenamePRIOR}, prior model = ${priorname}, lambda = ${lambda}] +++\n"
 
-$c python ${scriptdir}/cstAcqAlgos/main.py -a ${caalgo} -b ${instancenameCA} -paf -pf ${priorfile} > ${logfile}
+$c python ${scriptdir}/cstAcqAlgos/main.py -a ${caalgo} -b ${instancenameCA} -pam -lam ${lambda} -pf ${priorfile} > ${logfile}
 
 printf "+++ With prior DONE +++\n"
 
